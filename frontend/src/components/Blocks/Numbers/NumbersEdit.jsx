fc95bb851f5c399ee86265483b546339d436a428
Klesio dev <klesio.dev@gmail.com>
2023-09-23 21:51:35 -0300

    feat(project): added edit text component

--- frontend/src/components/Blocks/Numbers/NumbersEdit.jsx
+++ frontend/src/components/Blocks/Numbers/NumbersEdit.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import TextLineEdit from '../../../../omelette/src/components/manage/TextLineEdit/TextLineEdit';
 
 const NumbersEdit = (props) => {
-  return <div>só mais 72 horas</div>;
+  const { block, onChangeBlock, properties, isEditMode, data } = props;
+
+  if (data.title === undefined) {
+    onChangeBlock(block, {
+      ...data,
+      title: 'Countdown Timer',
+    });
+  }
+
+  return (
+    <div>
+      <TextLineEdit
+        {...props}
+        renderTag="div"
+        renderClassName="title"
+        fieldDataName="title"
+        placeholder="contagem regressiva"
+        data={data}
+        editable={isEditMode}
+        onChangeBlock={onChangeBlock}
+        properties={properties}
+      />
+    </div>
+  );
 };
 
 export default NumbersEdit;
--- frontend/src/components/Blocks/Numbers/NumbersView.jsx
+++ frontend/src/components/Blocks/Numbers/NumbersView.jsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const NumbersView = (props) => {
-  return <div>só mais 72 horas</div>;
+  const { data } = props;
+  const [timeRemaining, setTimeRemaining] = useState(3600);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        clearInterval(timer); // Parar o contador quando o tempo acabar
+      }
+    }, 1000);
+
+    // Limpando o intervalo quando o componente é desmontado
+    return () => clearInterval(timer);
+  }, [timeRemaining]);
+
+  const hours = Math.floor(timeRemaining / 3600);
+  const minutes = Math.floor((timeRemaining % 3600) / 60);
+  const seconds = timeRemaining % 60;
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="title text-4xl font-bold uppercase p-4">{data.title}</div>
+      <div>
+        <ul className="flex gap-4">
+          <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
+            <span className="">{hours.toString().padStart(2, '0')}</span>
+            <span>Days</span>
+          </li>
+          <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
+            <span className="">{minutes.toString().padStart(2, '0')}</span>
+            <span>Minutes</span>
+          </li>
+          <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
+            <span className="">{seconds.toString().padStart(2, '0')}</span>
+            <span>Seconds</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NumbersView;
