749733be904ebd17aa7c2b4882b798d4c58c7b03
Klesio dev <klesio.dev@gmail.com>
2023-09-24 00:29:01 -0300

    feat(product):  created countdown component

--- frontend/src/components/Blocks/Numbers/NumbersEdit.jsx
+++ frontend/src/components/Blocks/Numbers/NumbersEdit.jsx
@@ -1,29 +1,146 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TextLineEdit from '../../../../omelette/src/components/manage/TextLineEdit/TextLineEdit';
 
 const NumbersEdit = (props) => {
-  const { block, onChangeBlock, properties, isEditMode, data } = props;
+  const { block, onChangeBlock, isEditMode, data } = props;
 
-  if (data.title === undefined) {
+  // Verifica os se os valores são undefined e atribui valores padrão
+
+  if (
+    data.title === undefined ||
+    data.hours === undefined ||
+    data.minutes === undefined ||
+    data.seconds === undefined ||
+    data.hoursTime === undefined ||
+    data.minutesTime === undefined ||
+    data.secondsTime === undefined ||
+    data.buttonStart === undefined ||
+    data.buttonStop === undefined
+  ) {
     onChangeBlock(block, {
       ...data,
-      title: 'Countdown Timer',
+      title: 'Countdown Time',
+      hours: 'Hours',
+      minutes: 'Minutes',
+      seconds: 'Seconds',
+      hoursTime: '00',
+      minutesTime: '0',
+      secondsTime: '0',
+      buttonStart: 'Start',
+      buttonStop: 'Stop',
     });
   }
 
   return (
-    <div>
-      <TextLineEdit
-        {...props}
-        renderTag="div"
-        renderClassName="title"
-        fieldDataName="title"
-        placeholder="contagem regressiva"
-        data={data}
-        editable={isEditMode}
-        onChangeBlock={onChangeBlock}
-        properties={properties}
-      />
+    <div className="flex flex-col justify-center items-center">
+      <div className="text-4xl font-bold uppercase p-4">
+        <TextLineEdit
+          {...props}
+          renderTag="div"
+          renderClassName="title"
+          fieldDataName="title"
+          placeholder="contagem regressiva"
+          data={data}
+          editable={isEditMode}
+          onChangeBlock={onChangeBlock}
+        />
+      </div>
+
+      <div>
+        <ul className="flex gap-4">
+          <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              fieldDataName="hoursTime"
+              placeholder="0"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              fieldDataName="hours"
+              placeholder="Hours"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+          </li>
+          <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              fieldDataName="minutesTime"
+              placeholder="0"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              renderClassName="minutes"
+              fieldDataName="minutes"
+              placeholder="Minutes"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+          </li>
+          <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              renderClassName="secondsTime"
+              fieldDataName="secondsTime"
+              placeholder="0"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              renderClassName="seconds"
+              fieldDataName="seconds"
+              placeholder="Seconds"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+          </li>
+        </ul>
+        <div className="flex justify-center gap-4">
+          <span className="start bg-green-400 px-6 py-4 text-xl font-medium text-white">
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              renderClassName="buttonStart"
+              fieldDataName="buttonStart"
+              placeholder="Start"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+          </span>
+          <span className="stop bg-red-400 px-6 py-4 text-xl font-medium text-white">
+            <TextLineEdit
+              {...props}
+              renderTag="span"
+              renderClassName="buttonStop"
+              fieldDataName="buttonStop"
+              placeholder="Stop"
+              data={data}
+              editable={isEditMode}
+              onChangeBlock={onChangeBlock}
+            />
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/components/Blocks/Numbers/NumbersView.jsx
+++ frontend/src/components/Blocks/Numbers/NumbersView.jsx
@@ -1,44 +1,108 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const NumbersView = (props) => {
   const { data } = props;
-  const [timeRemaining, setTimeRemaining] = useState(3600);
+  const [hours, setHours] = useState(Number(data.hoursTime) || 0);
+  const [minutes, setMinutes] = useState(Number(data.minutesTime) || 0);
+  const [seconds, setSeconds] = useState(Number(data.secondsTime) || 0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  /*
+  faz a soma total de segundos para armazenar valores que precisam persistir
+  entre as renderizações do componente, garantido que o valor preservado
+  corretamente e não seja reinicializado a cada renderização
+  */
+
+  const totalSecondsRef = useRef(hours * 3600 + minutes * 60 + seconds);
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      if (timeRemaining > 0) {
-        setTimeRemaining(timeRemaining - 1);
-      } else {
-        clearInterval(timer); // Parar o contador quando o tempo acabar
-      }
-    }, 1000);
+    let timer;
 
-    // Limpando o intervalo quando o componente é desmontado
+    if (isRunning && totalSecondsRef.current > 0) {
+      timer = setInterval(() => {
+        if (totalSecondsRef.current > 0) {
+          totalSecondsRef.current -= 1;
+          setHours(Math.floor(totalSecondsRef.current / 3600));
+          setMinutes(Math.floor((totalSecondsRef.current % 3600) / 60));
+          setSeconds(totalSecondsRef.current % 60);
+        } else {
+          clearInterval(timer);
+          setIsRunning(false);
+        }
+      }, 1000);
+    }
+
     return () => clearInterval(timer);
-  }, [timeRemaining]);
+  }, [isRunning]);
 
-  const hours = Math.floor(timeRemaining / 3600);
-  const minutes = Math.floor((timeRemaining % 3600) / 60);
-  const seconds = timeRemaining % 60;
+  const handleStart = () => {
+    setIsRunning(true);
+  };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="title text-4xl font-bold uppercase p-4">{data.title}</div>
+    <div className="flex flex-col justify-center items-center my-20">
+      <div className="title text-4xl font-bold uppercase p-4">
+        {data.title === undefined ? 'Countdown Timer' : data.title}
+      </div>
       <div>
         <ul className="flex gap-4">
           <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
-            <span className="">{hours.toString().padStart(2, '0')}</span>
-            <span>Days</span>
+            <span
+              onChange={(e) => setHours(e.target.value)}
+              disabled={isRunning}
+              className="hoursTime"
+            >
+              {hours < 10 ? '0' + hours : hours}
+            </span>
+            <span className="hours">
+              {data.hours === undefined ? 'hours' : data.hours}
+            </span>
           </li>
           <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
-            <span className="">{minutes.toString().padStart(2, '0')}</span>
-            <span>Minutes</span>
+            <span
+              onChange={(e) => setMinutes(e.target.value)}
+              disabled={isRunning}
+              className="minutesTime"
+            >
+              {minutes < 10 ? '0' + minutes : minutes}
+            </span>
+            <span className="minutes">
+              {data.minutes === undefined ? 'minutes' : data.minutes}
+            </span>
           </li>
           <li className="flex flex-col gap-4 text-3xl p-4 uppercase font-semibold items-center">
-            <span className="">{seconds.toString().padStart(2, '0')}</span>
-            <span>Seconds</span>
+            <span
+              onChange={(e) => setSeconds(e.target.value)}
+              disabled={isRunning}
+              className="secondsTime"
+            >
+              {seconds < 10 ? '0' + seconds : seconds}
+            </span>
+            <span className="seconds">
+              {data.seconds === undefined ? 'seconds' : data.seconds}
+            </span>
           </li>
         </ul>
+        <div className="flex justify-center gap-4">
+          <button
+            onClick={handleStart}
+            disabled={isRunning}
+            className="buttonStart start bg-green-400 cursor-pointer px-6 py-4 text-xl font-medium text-white"
+          >
+            {data.buttonStart === undefined ? 'start' : data.buttonStart}
+          </button>
+          <button
+            onClick={handleStop}
+            disabled={!isRunning}
+            className="buttonStart stop bg-red-400 cursor-pointer px-6 py-4 text-xl font-medium text-white"
+          >
+            {data.buttonStop === undefined ? 'stop' : data.buttonStop}
+          </button>
+        </div>
       </div>
     </div>
   );
